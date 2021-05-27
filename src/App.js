ed6d7436583e22350cb0b627115d60fedb45e51e
seung <sjoh0704@daum.net>
2021-05-27 17:39:06 +0900

    split component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
+import TOC from './Components/TOC'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Subject extends Component{
+  render(){
+    return(
+      <header>
+            <h1>{this.props.title}</h1>
+            {this.props.subtitle} 
+        </header>
+        // 이런식으로 props를 설정하여 더 효율적으로 사용할 수 있다. 
+    )
+  }
+}
+
+
+class Contents extends Component{
+  render(){
+    return(  <article>
+      <h2>HTML</h2>
+      HTML is cindy!
+  </article>
+);
+  }
+}
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        hello world
+        {/* probs를 사용하면 다음처럼 사용할 수 있다. */}
+        <Subject title="WEB" subtitle="this is subtitle"></Subject>
+      
+        <TOC></TOC>
+        <Contents></Contents>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/Components/TOC.js
@@ -0,0 +1,19 @@
+// components 쪼개기 
+
+import React, {Component} from 'react'
+class TOC extends Component{
+    render(){
+        return (     
+           <nav>
+          <ul>
+              <li><a href="1.html">JavaScript</a></li>
+              <li><a href="2.html">JavaScript2</a></li>
+              <li><a href="3.html">JavaScript3</a></li>
+          </ul>
+        </nav>)
+      
+    }
+  }
+  
+
+  export default TOC 
